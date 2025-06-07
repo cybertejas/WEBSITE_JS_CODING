9a48942789d73633ca97d86d3f4281ae695af960
ArvinderPal10 <arvinderpal10@gmail.com>
2025-06-07 21:47:05 +0530

    Conditionals in Javascript

---
+++ Online/Conditionals/conditionals.js
@@ -0,0 +1,142 @@
+// Conditionals -- 
+//  Sequential -- Top to bottom -- manner 
+
+// if (condition){
+//     // Execute this code
+// }
+
+// if (5<3){
+//     console.log("The condition was true")
+// }
+// else{
+//     console.log("The condition was false")
+// }
+
+// x = 10
+// if (x<10){
+//     console.log("X is less than 10")
+// } else if (x>10){
+//     console.log("X is greater than 10")
+// } else{
+//     console.log("Print this if condition is false")
+// }
+
+// Switch Case 
+// day = 5
+
+// switch (day){
+//     case 1:
+//         console.log("Sunday")
+//         break
+//     case 2:
+//         console.log("Monday")
+//         break 
+//     case 3:
+//         console.log("Tuesday")
+//         break
+//     case 4:
+//         console.log("Wednesday")
+//         break
+//     case 5:
+//         console.log("Thursday")
+//         break
+//     case 6:
+//         console.log("Friday")
+//         break
+//     case 7:
+//         console.log("Saturday")
+//         break   
+//     default:
+//         console.log("Invalid day")
+// }
+
+
+// var fruit = "mango"
+
+// switch (fruit){
+//     case "mango":
+//         break
+//     case "pear":
+//         console.log("This is a fruit")
+//     default:
+//         console.log("Default")
+// }
+
+// Falsy and Truthy values in JS 
+
+// false , 0 , 0n , "", null , undefined , NaN 
+
+// if (false){
+//     console.log("Condtions is True")
+// }else{
+//     console.log("Condition is False")
+// }
+
+// if (0){
+//     console.log("Condtions is True")
+// }else{
+//     console.log("Condition is False")
+// }
+// if (NaN){
+//     console.log("Condtions is True")
+// }else{
+//     console.log("Condition is False")
+// }
+
+// if (undefined){
+//     console.log("Condtions is True")
+// }else{
+//     console.log("Condition is False")
+// }
+
+// if (null){
+//     console.log("Condtions is True")
+// }else{
+//     console.log("Condition is False")
+// }
+
+// if (""){
+//     console.log("Condtions is True")
+// }else{
+//     console.log("Condition is False")
+// }
+
+// Truthy -- 
+//true, any number +ve  , -ve (except zero) , Any string (except empty string)
+
+// Objects - [], {},
+// if ("Hello"){
+//     console.log("Condtions is True")
+// }else{
+//     console.log("Condition is False")
+// }
+
+// if (true){
+//     console.log("Condtions is True")
+// }else{
+//     console.log("Condition is False")
+// }
+
+// if (1234){
+//     console.log("Condtions is True")
+// }else{
+//     console.log("Condition is False")
+// }
+
+// if ([]){
+//     console.log("Condtions is True")
+// }else{
+//     console.log("Condition is False")
+// }
+
+// if ({}){
+//     console.log("Condtions is True")
+// }else{
+//     console.log("Condition is False")
+// }
+
+// Ternary Operator -- 
+
+// conditon ? DoThisiftrue:DoThisIfFalse
+
+5<6 ? console.log("True"):console.log("False")
