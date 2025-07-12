1b269a07f659ae6cf4a4165f371408147140eaea
ArvinderPal10 <arvinderpal10@gmail.com>
2025-07-12 21:58:13 +0530

    Patterns in Javascript

---
+++ Patterns/Star.js
@@ -0,0 +1,134 @@
+// 
+
+// * * * * * * * 
+// * * * * * * * 
+// * * * * * * * 
+// * * * * * * * 
+// * * * * * * * 
+// console.log("*")
+// console.log("* * * * * * *")
+// console.log("* * * * * * *")
+// console.log("* * * * * * *")
+// console.log("* * * * * * *")
+
+// for (let i=0;i<5;i++){
+//         console.log("* * * * * *")
+// }
+// pattern = ""
+// for (let x=0;x<5;x++){
+//     for (let i=0;i<5;i++){
+//         pattern+= "* "
+//     }
+//     pattern+="\n"
+// }
+
+// console.log(pattern)
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+// pattern = ""
+// for (let x=0;x<5;x++){
+//     for (let i=0;i<=x;i++){
+//         pattern+= "* "
+//     }
+//     pattern+="\n"
+// }
+
+// console.log(pattern)
+
+
+// * * * * *
+// * * * * 
+// * * *
+// * * 
+// *
+
+// pattern = ""
+// for (let x=5;x>0;x--){
+//     for (let i=1;i<=x;i++){
+//         pattern+= "* "
+//     }
+//     pattern+="\n"
+// }
+
+// console.log(pattern)
+
+// 1 
+// 2 2 
+// 3 3 3 
+// 4 4 4 4 
+// 5 5 5 5 5 
+
+
+// pattern = ""
+// for (let x=0;x<5;x++){
+//     for (let i=0;i<=x;i++){
+//         pattern+= x+1 + " "
+//     }
+//     pattern+="\n"
+// }
+
+// console.log(pattern)
+
+// 1 
+// 1 2 
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+// pattern = ""
+// for (let x=0;x<5;x++){
+//     for (let i=0;i<=x;i++){
+//         pattern+= i+1 + " "
+//     }
+//     pattern+="\n"
+// }
+
+// console.log(pattern)
+
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10
+// 11 12 13 14 15
+
+// pattern = ""
+// y =0
+// for (let x=0;x<5;x++){
+//     for (let i=0;i<=x;i++){
+//         pattern+= y+1 + " "
+//         y+=1   
+//     }
+//     pattern+="\n"
+  
+// }
+
+// console.log(pattern)
+
+// A 
+// B B 
+// C C C 
+// D D D D 
+// E E E E E
+
+
+// pattern = ""
+// for (let x=0;x<5;x++){
+//     for (let i=0;i<=x;i++){
+//         pattern+= String.fromCharCode(65+x)+ " "
+//     }
+//     pattern+="\n"
+// }
+
+// console.log(pattern)
+
+// A 
+// A B 
+// A B C 
+// A B C D 
+// A B C D E
+
